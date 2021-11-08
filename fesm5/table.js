f26f6731ba6d34d7208c69c795e3616c4a6d23a7
kevin violas <kevin.violas@nowbrains.com>
2021-11-08 17:29:42 +0100

    fix: test

--- fesm5/table.js
+++ fesm5/table.js
@@ -923,7 +923,7 @@ var CoreMatTable = /** @class */ (function (_super) {
                 finally { if (e_2) throw e_2.error; }
             }
             this.dataAfterSearch = result.filter((function (e) { return e.pond; })).sort(function (a, b) { return a > b ? 1 : (a < b ? -1 : 0); });
-            this.pageNumber.next(1);
+            //this.pageNumber.next(1);
             return result.filter((function (e) { return e.pond; })).sort(function (a, b) { return a > b ? 1 : (a < b ? -1 : 0); });
         }
         else {
