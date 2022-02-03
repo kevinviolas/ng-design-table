ce0bb8cbb71c58b488d2c9a041d75f1f097a43df
kevin violas <kevin.violas@nowbrains.com>
2022-02-03 15:12:11 +0100

    fix: test

--- fesm5/table.js
+++ fesm5/table.js
@@ -426,7 +426,7 @@ var NameAvatarComponent = /** @class */ (function () {
     NameAvatarComponent = __decorate([
         Component({
             selector: 'name-avatar',
-            template: "<div [ngStyle]=\"src && src.includes('assets') && {'display': 'none'}\" #avatar>\n    {{letter}}\n</div>\n\n<img [src]=\"src\" [ngStyle]=\"{'width': fontSize || '44px', 'height': fontSize || '44px', 'border-radius': _borderRadius}\" *ngIf=\"src && src.includes('assets')\" />",
+            template: "<div [ngStyle]=\"src && src.includes('assets') && {'display': 'none'}\" #avatar>\n    {{letter}}\n</div>\n\n<img [src]=\"src\" [ngStyle]=\"{'width': fontSize || '44px', 'height': fontSize || '44px',  'vertical-align': 'middle', 'border-radius': _borderRadius}\" *ngIf=\"src && src.includes('assets')\" />",
             styles: ["div{align-items:center;justify-content:center;padding:0!important}"]
         }),
         __metadata("design:paramtypes", [TableService])
