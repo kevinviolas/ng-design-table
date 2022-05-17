04c9f0494b936ee2f354783b0bd44524cb576cfa
Diary Narindra <diarynar@gmail.com>
2022-05-17 20:17:57 +0300

    fix: status module

--- lib/cells-component/status/status.component.d.ts
+++ lib/cells-component/status/status.component.d.ts
@@ -1,7 +1,7 @@
 import { OnInit } from '@angular/core';
 export declare class StatusComponent implements OnInit {
     label: string;
-    value: string;
+    class: string;
     constructor();
     ngOnInit(): void;
 }
--- lib/table.component.d.ts
+++ lib/table.component.d.ts
@@ -1,9 +1,9 @@
-import { ChangeDetectorRef, EventEmitter, OnChanges, OnInit, SimpleChanges } from '@angular/core';
-import { CellsComponentList } from './setting/CellsComponentRegistry';
-import { MatSort } from '@angular/material/sort';
-import { MatPaginator } from '@angular/material/paginator';
-import { CoreMatTable, CoreMatTableInterface, FilterDateInterface, Page, PageRequest, Sort } from './core-data-table';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ChangeDetectorRef, EventEmitter, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { CoreMatTable, CoreMatTableInterface, FilterDateInterface, Page, PageRequest, Sort } from './core-data-table';
+import { CellsComponentList } from './setting/CellsComponentRegistry';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 import { TableService } from './table.service';
 import { TranslateService } from './translate.service';
 interface displayColumnsConfig {
@@ -17,6 +17,7 @@ interface displayedColumnsInterface {
     ratio?: number;
     order?: number;
     class?: string;
+    category?: string;
     module?: CellsComponentList;
     override?: string | string[];
     display?: string;
