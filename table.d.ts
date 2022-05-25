cbfb3a8ecc8e79cd7e111b50ad436271c8a1a840
kevin violas <kevin.violas@nowbrains.com>
2022-05-25 12:02:07 +0200

    fix: test

--- table.d.ts
+++ table.d.ts
@@ -2,5 +2,6 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public-api';
+export { ButtonLinkTextComponent as ɵc } from './lib/cells-component/button-link-text/button-link-text.component';
 export { NgxFlagsComponent as ɵb } from './lib/cells-component/ngx-flag/ngx-flag.component';
 export { TranslateService as ɵa } from './lib/translate.service';
